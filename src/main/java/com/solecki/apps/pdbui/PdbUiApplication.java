ec3ab2cf988cc114582085371bd3f24c1ef53ad1
Grzegorz Solecki <gsolecki8@gmail.com>
2022-08-09 09:44:43 -0400

    Init version

---
+++ src/main/java/com/solecki/apps/pdbui/PdbUiApplication.java
@@ -0,0 +1,13 @@
+package com.solecki.apps.pdbui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PdbUiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PdbUiApplication.class, args);
+    }
+
+}
