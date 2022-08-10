344c338e487abbbd320a924fca9d4a95516dc495
Grzegorz Solecki <gsolecki8@gmail.com>
2022-08-10 11:04:11 -0400

    Add get code

--- src/main/java/com/solecki/apps/pdbui/Index.java
+++ src/main/java/com/solecki/apps/pdbui/Index.java
@@ -15,7 +15,37 @@ public class Index extends HttpServlet {
     private static final long serialVersionUID = 1L;
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        // empty
+        response.getWriter().write("""
+                <html>
+                <head>
+                <meta charset="ISO-8859-1">
+                <title>Eat More Cucumbers Restaurant</title>
+                </head>
+                <body>
+                <h2>Eat More Cucumbers Restaurant Freshville</h2>
+                 <form action = "Index" method = "POST" target = "">
+                 <table>
+                 <tr>
+                 <td>Bill Amount: </td>\s
+                 <td><input type = "text" id="billamount" name = "billamount"/></td>
+                </tr>
+
+                 <tr>
+                 <td>Tax Rate: </td>\s
+                 <td><input type = "text" id="taxrate" name = "taxrate"/> \s
+                 </td>
+                 </tr>
+                 <tr>
+                 <td></td>
+                 <td>
+                 <br> <input type = "submit" id="mybutton" value = "Calculate Final Bill" />
+                  </td>
+                  </tr>
+                  </table>
+                      </form>
+                </body>
+                </html>                        
+                """);
     }
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
@@ -26,18 +56,7 @@ public class Index extends HttpServlet {
 
             double finalBillAmount = BillCalculationHelper.calculateBillForCustomer(billAmount, taxRate);
 
-            String lipton = """
-                    
-                    """;
-
-            response.getWriter().write("""
-                            """
-
-                    "<html><body><h1>"
-                    + "Initial Bill is: $" + billAmount + "</br>"
-                    + "Tax Rate is: " + taxRate + "%" + "</br>"
-                    + "Final Bill Amount is: $" + finalBillAmount + "</br>"
-                    + "</h1></body></html>");
+            response.getWriter().write("<html><body><h1>" + "Initial Bill is: $" + billAmount + "</br>" + "Tax Rate is: " + taxRate + "%" + "</br>" + "Final Bill Amount is: $" + finalBillAmount + "</br>" + "</h1></body></html>");
 
         } catch (Exception ex) {
             response.getWriter().write("Unknown Error: " + ex.getMessage());
